b262be0bc9cac69a9c6569f76577ddc2a845f713
Martin Tasker <martin.tasker@databatix.com>
2024-04-09 16:54:52 +0100

    tiles: split out svg_writer

---
+++ src/svg_writer.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use tesselation::Point2d;
+
+pub struct SVGWriter {
+  file: File,
+}
+
+const SVG_TOP: &str = r#"
+<svg viewBox="-5 -5 10 10" xmlns="http://www.w3.org/2000/svg" width="1000" height="1000">
+  <g transform="scale(1 -1)">
+"#;
+const SVG_TAIL: &str = r#"
+  </g>
+</svg>
+"#;
+
+const POLYGON_TOP: &str ="    <polygon points=\"";
+const POLYGON_TAIL: &str = "\" stroke-width=\"0.2%\" stroke=\"black\" fill=\"lightgrey\" />";
+
+fn svg_polygon(points: Vec<Point2d>) -> String {
+  let mut res: String = String::from(POLYGON_TOP);
+  let points_str: Vec<String> = points.iter().map(|p| format!("{},{}", p.0, p.1)).collect();
+  res.push_str(&points_str.join(" "));
+  res.push_str(POLYGON_TAIL);
+  res
+}
+
+impl SVGWriter {
+  pub fn new(filename: &str) -> std::io::Result<Self> {
+    Ok(Self { file: File::create(filename)? })
+  }
+
+  pub fn write_model(&mut self, model: Vec<Point2d>) -> std::io::Result<()> {
+    self.file.write_all(SVG_TOP.as_bytes())?;
+    self.file.write_all(svg_polygon(model).as_bytes())?;
+    self.file.write_all(SVG_TAIL.as_bytes())?;
+    Ok(())
+  }
+}
