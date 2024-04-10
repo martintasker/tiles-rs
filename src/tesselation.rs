c7936e2069d9b3e5809fc76ec73b4ae1b6d0ba2e
Martin Tasker <martin.tasker@databatix.com>
2024-04-10 12:05:04 +0100

    tiles: use Rust builder pattern

--- src/tesselation.rs
+++ src/tesselation.rs
@@ -4,27 +4,47 @@ use omega8::get_square;
 
 pub type Tile = Vec<Point8>; // use in the model
 
+struct Tesselation {
+  tiles: Vec<Tile>,
+}
+
+struct TesselationBuilder(Tesselation);
+
+impl TesselationBuilder {
+  fn new() -> Self {
+    Self(Tesselation{tiles: Vec::new()})
+  }
+
+  fn build(self) -> Tesselation {
+    self.0
+  }
+
+  fn add_tile(&mut self, tile: &Tile) -> &mut Self {
+    self.0.tiles.push(tile.clone());
+    self
+  }
+}
+
 pub fn get_model() -> Vec<Tile> {
-  let mut res: Vec<Tile> = Vec::new();
+  let mut builder = TesselationBuilder::new();
 
   // construct an initial central octagon
   let centre_octagon = get_octagon(Point8{x: [-1, 0], y: [-1, -1]}, 0);
-  res.push(centre_octagon.clone());
+  builder.add_tile(&centre_octagon);
 
   // construct a set of squares surrounding the central octagon, on diagonals
   for [point_index, initial_direction] in [[1, 7], [3, 1], [5, 3], [7, 5]] {
     let anchor_point: Point8 = centre_octagon[point_index];
     let adjacent_square = get_square(anchor_point, initial_direction);
-    res.push(adjacent_square);
+    builder.add_tile(&adjacent_square);
   }
 
   // construct a set of octagons surrounding the central octagon
   for [point_index, initial_direction] in [[2, 7], [4, 1], [6, 3], [0, 5]] {
     let anchor_point: Point8 = centre_octagon[point_index];
     let adjacent_octagon = get_octagon(anchor_point, initial_direction);
-    res.push(adjacent_octagon);
+    builder.add_tile(&adjacent_octagon);
   }
 
-  // that's it
-  res
+  builder.build().tiles
 }
