2e53a81c29feca692fc7d188786424d708c21221
Martin Tasker <martin.tasker@databatix.com>
2024-04-12 18:09:01 +0100

    tiles: more genericization

--- src/omega_coords.rs
+++ src/omega_coords.rs
@@ -1,5 +1,9 @@
 pub type OmegaSubCoords<const N: usize> = [i32; N];
-pub type OmegaSubCoordBasis<const N: usize> = [f64; N];
+
+pub struct OmegaSubCoordBasis<const N: usize> {
+  pub coefficients: [f64; N],
+  pub divisor: i32,
+}
 
 pub fn sub_coord_sum<const N: usize>(a: &OmegaSubCoords<N>, b: &OmegaSubCoords<N>) -> OmegaSubCoords<N> {
   let mut res = a.clone();
@@ -12,9 +16,9 @@ pub fn sub_coord_sum<const N: usize>(a: &OmegaSubCoords<N>, b: &OmegaSubCoords<N
 pub fn coord_projected<const N: usize>(coords: &OmegaSubCoords<N>, basis: &OmegaSubCoordBasis<N>) -> f64 {
   let mut res = 0.0;
   for i in 0..N {
-    res += coords[i] as f64 * basis[i];
+    res += coords[i] as f64 * basis.coefficients[i];
   }
-  res
+  res / (basis.divisor as f64)
 }
 
 pub struct OmegaVector<const N: usize> {
@@ -22,9 +26,17 @@ pub struct OmegaVector<const N: usize> {
   pub dy: OmegaSubCoords<N>,
 }
 
+#[derive(Debug)]
 pub struct OmegaPoint<const N: usize> {
   pub x: OmegaSubCoords<N>,
   pub y: OmegaSubCoords<N>,
+}
+
+impl<const N: usize> OmegaPoint<N> {
+  #[allow(dead_code)]
+  pub fn origin() -> Self {
+    Self{x: [0; N], y: [0; N]}
+  }
 }
 
 pub struct XYPoint {
@@ -51,12 +63,18 @@ impl<const N: usize> OmegaPoint<N> {
 
 pub struct OmegaSpace<const N_DIRECTIONS: usize, const BASIS_SIZE: usize> {
   pub unit_vectors: [OmegaVector<BASIS_SIZE>; N_DIRECTIONS],
+  pub basis: OmegaSubCoordBasis<BASIS_SIZE>,
 }
 
 impl<const N_DIRECTIONS: usize, const BASIS_SIZE: usize> OmegaSpace<N_DIRECTIONS, BASIS_SIZE> {
   #[allow(dead_code)]
   pub fn n_directions(self) -> usize {
     return N_DIRECTIONS;
+  }
+
+  #[allow(dead_code)]
+  pub fn direction_plus(&self, direction: usize, delta: i32) -> usize {
+    (((direction + N_DIRECTIONS) as i32 + delta) as usize) % N_DIRECTIONS
   }
 }
 
@@ -65,13 +83,38 @@ mod tests {
   use super::*;
 
   #[test]
+  fn test_direction_plus() {
+    let space: OmegaSpace<12, 0> = OmegaSpace{
+      unit_vectors: core::array::from_fn(|_| OmegaVector{dx: [], dy: []}),
+      basis: OmegaSubCoordBasis{ coefficients: [], divisor: 2},
+    };
+    assert_eq!(space.direction_plus(0, 1), 1);
+    assert_eq!(space.direction_plus(0, -1), 11);
+    assert_eq!(space.direction_plus(8, 5), 1);
+    assert_eq!(space.direction_plus(3, -5), 10);
+  }
+
+  #[test]
   fn test_sub_coord_sum() {
     assert_eq!(sub_coord_sum(&[1, 2], &[3, 4]), [4, 6]);
   }
 
   #[test]
   fn test_coord_projected() {
-    assert_eq!(coord_projected(&[1, 2], &[1.0, 1.25]), 3.5);
+    assert_eq!(
+      coord_projected(
+        &[1, 2],
+        &OmegaSubCoordBasis{coefficients: [1.0, 1.25], divisor: 1}
+      ),
+      3.5
+    );
+    assert_eq!(
+      coord_projected(
+        &[1, 2],
+        &OmegaSubCoordBasis{coefficients: [1.0, 1.25], divisor: 2}
+      ),
+      1.75
+    );
   }
 
   #[test]
--- src/omega_space.rs
+++ src/omega_space.rs
@@ -4,10 +4,10 @@ pub const SQRT2: f64 = std::f64::consts::SQRT_2;
 pub const SQRT3: f64 = 1.7320508075688772935274463;
 
 #[allow(dead_code)]
-pub const OMEGA8_SUB_COORD_BASIS: OmegaSubCoordBasis<2> = [
-  0.0,
-  SQRT2,
-];
+pub const OMEGA8_SUB_COORD_BASIS: OmegaSubCoordBasis<2> = OmegaSubCoordBasis{
+  coefficients: [1.0, SQRT2],
+  divisor: 2
+};
 #[allow(dead_code)]
 pub const OMEGA8_UNIT_VECTORS: [OmegaVector<2>; 8] = [
   OmegaVector {dx: [2, 0], dy: [0, 0]},
@@ -21,10 +21,10 @@ pub const OMEGA8_UNIT_VECTORS: [OmegaVector<2>; 8] = [
 ];
 
 #[allow(dead_code)]
-pub const OMEGA12_SUB_COORD_BASIS: OmegaSubCoordBasis<2> = [
-  0.0,
-  SQRT3,
-];
+pub const OMEGA12_SUB_COORD_BASIS: OmegaSubCoordBasis<2> = OmegaSubCoordBasis{
+  coefficients: [1.0, SQRT3],
+  divisor: 2
+};
 #[allow(dead_code)]
 pub const OMEGA12_UNIT_VECTORS: [OmegaVector<2>; 12] = [
   OmegaVector {dx: [2, 0], dy: [0, 0]},
@@ -42,15 +42,43 @@ pub const OMEGA12_UNIT_VECTORS: [OmegaVector<2>; 12] = [
 ];
 
 #[allow(dead_code)]
-pub fn get_omega8() -> OmegaSpace<8, 2> {
+pub fn get_omega8_space() -> OmegaSpace<8, 2> {
   return OmegaSpace {
     unit_vectors: OMEGA8_UNIT_VECTORS,
+    basis: OMEGA8_SUB_COORD_BASIS,
   };
 }
 
 #[allow(dead_code)]
-pub fn get_omega12() -> OmegaSpace<12, 2> {
+pub fn get_omega12_space() -> OmegaSpace<12, 2> {
   return OmegaSpace {
     unit_vectors: OMEGA12_UNIT_VECTORS,
+    basis: OMEGA12_SUB_COORD_BASIS,
   };
+}
+
+#[cfg(test)]
+mod tests {
+  use omega_coords::OmegaPoint;
+  use super::*;
+
+  #[test]
+  fn test_omega8() {
+    let space = get_omega8_space();
+    let origin: OmegaPoint<2> = OmegaPoint::origin();
+    let target = origin.plus(&space.unit_vectors[2]);
+    let target_xy = target.to_xy_point(&space.basis);
+    assert_eq!(target_xy.x, 0.0);
+    assert_eq!(target_xy.y, 1.0);
+  }
+
+  #[test]
+  fn test_omega12() {
+    let space = get_omega12_space();
+    let origin: OmegaPoint<2> = OmegaPoint::origin();
+    let target = origin.plus(&space.unit_vectors[9]);
+    let target_xy = target.to_xy_point(&space.basis);
+    assert_eq!(target_xy.x, 0.0);
+    assert_eq!(target_xy.y, -1.0);
+  }
 }
