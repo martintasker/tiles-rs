6a996a74e722f5372c3ef36fef7c285316bfb5a7
Martin Tasker <martin.tasker@databatix.com>
2024-05-06 08:53:19 +0100

    tiles: brute-force new square

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,26 @@ mod shapes;
 use svg_writer::SVGWriter;
 use point2d::{get_point2d_list_from12, get_point2d_list_from8, is_all_inside};
 
+use omega_shapes::get_square;
+use omega_space::{OMEGA8_SPACE, OmegaSpacePoint};
+use omega_tiles::{Tile, Tesselation};
+use point2d::Point2d;
+
 fn main() -> std::io::Result<()> {
+  {
+    let mut writer = SVGWriter::new("out-new-square.svg")?;
+    let square = get_square(OmegaSpacePoint::new(&OMEGA8_SPACE), 0);
+    let tile0 = Tile::new(&square);
+    let mut tesselation: Tesselation<8, 2> = Tesselation::new();
+    tesselation.add(tile0);
+    let new_square_model_point2d = tesselation
+      .get_tiles()
+      .iter()
+      .map(get_point2d_list_from_tile)
+      .filter(is_all_inside)
+      .collect();
+    writer.write_model(new_square_model_point2d)?;
+  }
   {
     let mut writer = SVGWriter::new("out-spectre.svg")?;
     let spectre_model = spectre_model::get();
@@ -35,4 +54,17 @@ fn main() -> std::io::Result<()> {
     writer.write_model(model12_point2d)?;
   }
   Ok(())
+}
+
+fn get_point2d_list_from_tile<'a>(tile: &Tile<'a, 8, 2>) -> Vec<Point2d> {
+  return tile
+    .get_points()
+    .iter()
+    .map(get_point2d_from_omega8)
+  .collect();
+}
+
+fn get_point2d_from_omega8<'a>(point: &OmegaSpacePoint<'a, 8, 2>) -> Point2d {
+  let xy_point = point.to_xy_point();
+  return Point2d(xy_point.x, xy_point.y);
 }
